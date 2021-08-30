39854ca4356562dd3c561ae8f40237bbf8c67e92
RaghavGirgaonkar <raghav.girgaonkar@gmail.com>
2021-08-30 18:47:01 +0530

    Minor changes

--- profile_comparison.sh
+++ profile_comparison.sh
@@ -3,18 +3,21 @@
 source /Data/bcj/INPTA/soft/Pulsar/pulsarbashrc.sh
 source /home/asusobhanan/Work/V6/pinta.bashrc
 
-python3.6 -m pip install --user --upgrade pip
-python3.6 -m pip install --user scipy numpy lmfit
-python3.6 -m pip install --user statsmodels seaborn pandas importlib_metadata
+#python3.6 -m pip install --user --upgrade pip
+#python3.6 -m pip install --user scipy numpy lmfit
+#python3.6 -m pip install --user statsmodels seaborn pandas importlib_metadata
 
 #read -p "Enter fits1 :" fits1
 #read -p "Enter fits2 :" fits2
 
 declare -a asciifiles=()
 
+echo "*****************************************************"
+echo "Running Profile Comparison Test..."
+echo "*****************************************************"
 for i in $@
 do
-        pam -DFT -e fits.scrunched --setnchn 8 --setnbin 64 -r 0.5 ${i}
+        pam -DFT -e fits.scrunched --setnbin 64 -r 0.5 ${i}
 	pdv -jDFT -t ${i}.scrunched  | grep -v File | awk '{print $4}' > ${i}.scrunched.txt
 	asciifiles+=(${i}.scrunched.txt)
 done
