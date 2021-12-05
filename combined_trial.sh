5a0aa66546f699e11a53265b7e82d2def6840ef8
Nikita Agarwal <62657395+Nikita-Agarwal0509@users.noreply.github.com>
2021-12-05 13:30:37 +0530

    Created combined_trial.sh for trial

--- combined_trial.sh
+++ combined_trial.sh
@@ -20,10 +20,10 @@ echo "*****************************************************"
 for i in $@
 do
         pam -DFT -e fits.scrunched --setnbin 64 -r 0.5 ${i}
-	pdv -jDFT -t ${i}.scrunched  | grep -v File | awk '{print $4}' > ${i}.scrunched.txt
-	asciifiles+=(${i}.scrunched.txt)
+        pdv -jDFT -t ${i}.scrunched  | grep -v File | awk '{print $4}' > ${i}.scrunched.txt
+        asciifiles+=(${i}.scrunched.txt)
 done
- 
+
 python3.6 profilecomparision.py ${asciifiles[0]} ${asciifiles[1]}
 
 exit_code=$?
@@ -43,18 +43,18 @@ declare -i c=-1
 declare -a filenames=()
 for i in $@
 do
-z=$((c+1))
-	#echo $z
-	#echo $i
+        z=$((c+1))
+        #echo $z
+        #echo $i
         outfile=$(basename $i).txt
-	filenames+=($outfile)
-	echo "Filename is"
-	echo ${filenames[@]}
-	echo "Outfile is"
-	echo $outfile
+        filenames+=($outfile)
+        echo "Filename is"
+        echo ${filenames[@]}
+        echo "Outfile is"
+        echo $outfile
         pdmp -mc 64 -g ./$i.summary.ps/cps ./$i > $outfile
-	c=$((c+1))
-	
+        c=$((c+1))
+
 done
 echo "Going through array of .txt files"
 
@@ -62,10 +62,7 @@ echo "Going through array of .txt files"
 
 for j in "${filenames[@]}"
 do
-	awk '/Best DM/ {print $4" "$7}' $j >> DMs.txt
-
-
-
+        awk '/Best DM/ {print $4" "$7}' $j >> DMs.txt
 done
 
 python3 dmdiff.py
@@ -80,5 +77,10 @@ echo "*****************************************************"
 echo "Running Timing and Residual Test..."
 echo "*****************************************************"
 
+for i in $@
 
-python3.6 tim_res.py
+do
+        asciifiles+=(${i})
+done
+
+python3.6 tim_res.py ${asciifiles[0]} ${asciifiles[1]}
